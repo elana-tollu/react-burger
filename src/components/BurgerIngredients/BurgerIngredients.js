1eccfa912f272eed5ac736cb7d0640a85d314618
Elana Tollu <elana.tollu@yandex.ru>
2022-02-24 18:39:11 +0200

    remove x-scroll

--- src/components/BurgerIngredients/BurgerIngredients.js
+++ src/components/BurgerIngredients/BurgerIngredients.js
@@ -1,31 +1,8 @@
 import React from 'react';
 import styles from './BurgerIngredients.module.css';
 import TabPanel from '../TabPanel/TabPanel';
-import IngredientDetails from '../IngredientDetails/IngredientDetails';
-import {data} from '../utils/data.js'
-
-//function ingredientsOfType (ings, type) {
-//  return ings.filter(element => element.type === type);
-//};
 
 function BurgerIngredients () {
-
-  //const buns = ingredientsOfType(data, 'bun');
-  //const mains = ingredientsOfType(data, 'main');
-  //const sauces = ingredientsOfType(data, 'sauce');
-
-  //const listBuns = buns.map((ing, index) =>
-  //      <IngredientDetails key={index} ing={ing}/>
-  //    );
-
-  //const listMains = mains.map((ing, index) =>
-  //    <IngredientDetails key={index} ing={ing}/>
-  //  );
-
-  //const listSauces = sauces.map((ing, index) =>
-  //  <IngredientDetails key={index} ing={ing}/>
-  //);
-
   return (
     <section className={styles.BurgerIngredients}>
       <p className="text text_type_main-large">
@@ -35,11 +12,6 @@ function BurgerIngredients () {
       <div style={{ backgroundColor: '#4c4cff' }} className="mb-10 mt-5">
         <TabPanel></TabPanel>
       </div>
-
-      <div className={styles.ingredientsScroll}>
-
-      </div>
-
     </section>    
   );
 }
