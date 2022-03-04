38d5d74333b10515ddaee68f35b2b78204ff0849
Elana Tollu <elana.tollu@yandex.ru>
2022-03-04 14:37:51 +0200

    fix AppHeader logo and buttons

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -5,31 +5,36 @@ import React from 'react';
 function AppHeader() {
   return (
     <section className={styles.AppHeader}>
+
+      <div className={styles.logo}><Logo/></div>
+
       <nav className={styles.content}>
         <div className={styles.menu}>
           <Button type="secondary" size="small">
             <div className={styles.buttonContent}>
               <BurgerIcon type="primary" /> 
-              <p className="text text_type_main-default text_color_inactive">
+              <p style={{ marginLeft: '8px' }} className="text text_type_main-default text_color_inactive">
                 Конструктор
               </p>
             </div>
           </Button>
           
-          <Button type="secondary" size="medium">
+          <Button type="secondary" size="small">
             <div className={styles.buttonContent}>
               <ListIcon type="primary" />
+              <p style={{ marginLeft: '8px' }} className="text text_type_main-default text_color_inactive">
               Лента заказов
+              </p>
             </div>
           </Button>
         </div>
 
-        <Logo/>
-
-        <Button type="secondary" size="medium">
+        <Button type="secondary" size="small">
           <div className={styles.buttonContent}>
             <ProfileIcon type="primary" />
-            Личный кабинет
+            <p style={{ marginLeft: '8px' }} className="text text_type_main-default text_color_inactive">
+              Личный кабинет
+            </p>
           </div>
         </Button>
       </nav>
