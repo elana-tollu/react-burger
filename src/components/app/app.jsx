3c3068746aa60a955f63b1bd8254922e79ee6b3f
Elana Tollu <elana.tollu@yandex.ru>
2022-03-21 19:29:30 +0200

    extract api

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import AppHeader from '../app-header/app-header.jsx';
 import BurgerConstructor from '../burger-constructor/burger-constructor.jsx';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients.jsx';
+import * as api from '../../utils/api.js';
 
 import styles from './app.module.css';
 
@@ -10,10 +11,12 @@ function App() {
   const [ingredients, setIngredients] = React.useState([]);
 
   useEffect(() => {
-    loadIngredientCards()
-    .then( ingredientsData => {
-      setIngredients(ingredientsData);
-  })}, []);
+    api.loadIngredientCards()
+      .then(ingredientsData => {
+        setIngredients(ingredientsData);
+      })
+      .catch(alert)
+  }, []);
 
   return (
     <div className={styles.app}>
@@ -34,40 +37,3 @@ export default App;
 
 
 
-const baseUrl = 'https://norma.nomoreparties.space/api/';
-
-function request( method, endpoint, data ) {
-  return fetch(`${baseUrl}${endpoint}`, {
-      method,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-  }).then((response) => {
-      if (response.ok) {
-          return response.json();
-      } else {
-          return Promise.reject(
-              `Ошибка: ${response.status} ${response.statusText}`
-          );
-      }
-  });
-}
-
-function loadIngredientCards() {
-  return request('GET', 'ingredients')
-  .then((cards) => {
-      return cards.data.map((card) => ({
-          id: card._id,
-          name: card.name,
-          type:  card.type,
-          proteins:  card.proteins,
-          fat:  card.fat,
-          carbohydrates:  card.carbohydrates,
-          calories:  card.calories,
-          price:  card.price,
-          image:  card.image,
-      }));
-  })
-  .catch(alert);
-}
