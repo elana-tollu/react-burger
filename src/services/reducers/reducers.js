650963e0ddf80082d8e3f5eb1e6428e1aec31f90
Elana Tollu <elana.tollu@yandex.ru>
2022-05-01 00:02:20 +0300

    add reducer

--- src/services/reducers/reducers.js
+++ src/services/reducers/reducers.js
@@ -1,4 +1,32 @@
+import {
+    HIDE_INGREDIENT, 
+    LOAD_INGREDIENTS, 
+    SHOW_BURGER_INGREDIENTS, 
+    SHOW_INGREDIENT, 
+    SUBMIT_ORDER 
+} from "../actions/actions";
+
 export const rootReducer = (state, action) => {
     console.log(action);
-    return state;
+
+    switch (action.type) {
+        case LOAD_INGREDIENTS:
+            return state;
+        case SHOW_BURGER_INGREDIENTS:
+            return state;
+        case SHOW_INGREDIENT:
+            return {
+                ...state, 
+                currentIngredient: action.ingredient,
+            };
+        case HIDE_INGREDIENT:
+            return {
+                ...state, 
+                currentIngredient: null,
+            };
+        case SUBMIT_ORDER:
+            return state;
+        default:
+            return state;
+    }
 }
