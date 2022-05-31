83313d39ab4eb186b62e5bee5cd419486ccdf7aa
Elana Tollu <elana.tollu@yandex.ru>
2022-05-31 20:18:11 +0300

    add fake signIn

--- src/components/page-login/page-login.jsx
+++ src/components/page-login/page-login.jsx
@@ -3,19 +3,23 @@ import { Link } from 'react-router-dom';
 import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import { YandexEmailInput } from 'yandex/yandex-email-input';
 import { YandexPasswordInput } from 'yandex/yandex-password-input';
+import {useAuth} from 'services/auth';
 
 import styles from './page-login.module.css';
 
 function PageLogin () {
+    const {signIn} = useAuth();
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
 
-    const logIn = () => {
-        console.log("что-угодно")
+    const logIn = (event) => {
+        event.preventDefault();
+        signIn(console.log);
     };
     return (
         <section className={styles.body}>
-            <form className={styles.form}>
+            <form className={styles.form}
+                onSubmit={logIn}>
                 <div className={styles.inputsContainer}>
                     <h1 className={styles.title}>
                         <p className="text text_type_main-medium">Вход</p>
@@ -31,8 +35,7 @@ function PageLogin () {
                         <Button type="primary" 
                             size="medium" 
                             style={{ height: '56px' }} 
-                            className="ml-1 mr-1 mb-1 mt-6"
-                            onClick={logIn}>
+                            className="ml-1 mr-1 mb-1 mt-6">
                             Войти
                         </Button>
                     </div>
